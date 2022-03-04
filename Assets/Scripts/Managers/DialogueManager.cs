9e6dfb80b01702a6474d32330a820eae6225220f
Srayan Jana <valorzard.kiko@outlook.com>
2022-03-04 13:08:01 -0800

    starting on dialogue system

---
+++ Assets/Scripts/Managers/DialogueManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    //public string jsonString;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //jsonString = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
