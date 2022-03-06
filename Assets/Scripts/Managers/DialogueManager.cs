16612c17fa76d216fd27086667603ae75bbbd144
Srayan Jana <valorzard.kiko@outlook.com>
2022-03-06 15:30:22 -0800

    got text change working

--- Assets/Scripts/Managers/DialogueManager.cs
+++ Assets/Scripts/Managers/DialogueManager.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+// Srayan Jana - main coder
 
 public class DialogueManager : MonoBehaviour
 {
     //public string jsonString;
-
+    public GameObject textMesh;
     // Start is called before the first frame update
     void Start()
     {
         //jsonString = "";
+        //textMesh = GameObject.Find("Text").GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ButtonPressed()
+    {
+        //Debug.Log("AHHHHHH");
+        textMesh.GetComponent<Text>().text = "pog";
     }
 }
